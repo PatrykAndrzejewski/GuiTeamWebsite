4f83845cfa3f0c6adf915ff691680d75e49086c2
Darragh Carter <dccarter450@gmail.com>
2026-04-30 13:17:18 +0100

    Elements to grab from HTML

--- tic tac toe/tic tac toe js.js
+++ tic tac toe/tic tac toe js.js
@@ -1,20 +1,5 @@
-let playerO = "O";
-let playerX = "X";
-let curentPlayer = playerO;
-//               0  1  2  3  4  5  6  7  8
-let gameBoard = ["","","","","","","","",""];
-let gameCells;
-
-window.onload = function() {
-    gameCells = document.getElementsByClassName("game-cell");
-    for (let celll of gameCells)    {
-        celll.addEventListener("click", placeCell);
-    }
-}
-
-function placeCell() {
-
-    this.innerText = curentPlayer;
-
-    curentPlayer = (curentPlayer == playerO) ? playerX : playerO;
-}
+//grab elements from the DOM
+const gameBoard = document.getElementById('game-board');
+const cells = document.querySelectorAll('.cell');
+const gameStatus = document.getElementById('game-status');
+const restartButton = document.getElementById('restart-button');
