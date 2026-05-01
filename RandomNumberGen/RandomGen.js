9a9fcebe2cafed6acc2870df5e92b3d928f7e513
Darragh Carter <dccarter450@gmail.com>
2026-05-01 12:53:07 +0100

    Added game state variable

--- RandomNumberGen/RandomGen.js
+++ RandomNumberGen/RandomGen.js
@@ -3,3 +3,9 @@ const submitGuessButton = document.getElementById('submit-guess');
 const messageDisplay = document.getElementById('message');
 const attemptsDisplay = document.getElementById('attempts');
 const restartButton = document.getElementById('restart-button');
+
+let randomNumber;
+let attempts = 0;
+let gameActive = true;
+const minNum = 1;
+const maxNum = 100; //open to change number whenever
