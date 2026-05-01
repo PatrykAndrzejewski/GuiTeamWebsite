e8a83ac84dbc5ec19bb892ac20d62223bd56e54f
Darragh Carter <dccarter450@gmail.com>
2026-05-01 11:25:08 +0100

    added DOM elements

---
+++ RPS/RPS.js
@@ -0,0 +1,6 @@
+const playerChoiceDisplay = document.getElementById('player-choice');
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const resultDisplay = document.getElementById('result');
+const scoreDisplay = document.getElementById('score');
+const buttons = document.querySelectorAll('.rps-button');
+const restartButton = document.getElementById('restart-button');
