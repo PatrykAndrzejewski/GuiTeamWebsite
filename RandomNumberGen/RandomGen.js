bb6ac59ff505597059fa606919acd37be5fd655c
Darragh Carter <dccarter450@gmail.com>
2026-05-01 12:50:50 +0100

    started js added constants DOM elements

---
+++ RandomNumberGen/RandomGen.js
@@ -0,0 +1,5 @@
+const guessInput = document.getElementById('guess-input');
+const submitGuessButton = document.getElementById('submit-guess');
+const messageDisplay = document.getElementById('message');
+const attemptsDisplay = document.getElementById('attempts');
+const restartButton = document.getElementById('restart-button');
