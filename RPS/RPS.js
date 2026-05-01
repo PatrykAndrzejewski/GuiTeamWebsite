6cf7959d6d34afbebe34fc719e97e044fad8df6b
Darragh Carter <dccarter450@gmail.com>
2026-05-01 11:35:09 +0100

    added game state variables

--- RPS/RPS.js
+++ RPS/RPS.js
@@ -4,3 +4,7 @@ const resultDisplay = document.getElementById('result');
 const scoreDisplay = document.getElementById('score');
 const buttons = document.querySelectorAll('.rps-button');
 const restartButton = document.getElementById('restart-button');
+
+let playerScore = 0;
+let computerScore = 0;
+const choices = ['rock', 'paper', 'scissors'];
